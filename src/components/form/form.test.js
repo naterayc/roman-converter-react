cc999ec84e48548e5a5373902e0d1aa4bf71394d
naterayc <yndinatera10@gmail.com>
2022-05-19 11:55:18 -0400

    refactor test, ignore coverage index.js

--- src/components/form/form.test.js
+++ src/components/form/form.test.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Form from './form';
 
+
+//arrange
+beforeEach(() => render(<Form />));
 describe('given the form component, when its mounted', () => {
     it('should exits an input', () => {
-        //arrange
-        render(<Form />);
         //act
         const input = screen.getByPlaceholderText(/número/i);
+
         //assert
         expect(input).toBeInTheDocument();
     });
 
     it('should exits a submit button', () => {
-        //arrange
-        render(<Form />);
         //act
         const btn = screen.getByRole('button', { name: /convertir/i });
+
         //assert
         expect(btn).toBeInTheDocument();
     });
@@ -24,9 +25,6 @@ describe('given the form component, when its mounted', () => {
 
 describe('given the form component, when the user submit the form', () => {
     it('if the input is empty it should renders an error message', () => {
-        //arrange
-        render(<Form />);
-
         ////// compruebo que el mensaje no exista antes del evento ////
         expect(screen.queryByText(/debes ingresar un número válido/i)).not.toBeInTheDocument();
         ////// compruebo que el mensaje no exista antes del evento ////
@@ -35,14 +33,12 @@ describe('given the form component, when the user submit the form', () => {
         const btn = screen.getByRole('button', { name: /convertir/i });
         fireEvent.click(btn);
         const errorMsg = screen.queryByText(/debes ingresar un número válido/i);
+
         //assert
         expect(errorMsg).toBeInTheDocument();
     });
 
-    it('for the entry 6 it should renders a span VI', () => {
-        //arrange
-        render(<Form />);
-
+    it('for the entry 6 it should renders a heading with the text VI', () => {
         ////// compruebo que el mensaje no exista antes del evento ////
         expect(screen.queryByText(/vi/i)).not.toBeInTheDocument();
         ////// compruebo que el mensaje no exista antes del evento ////
@@ -52,16 +48,14 @@ describe('given the form component, when the user submit the form', () => {
         const btn = screen.getByRole('button', { name: /convertir/i });
         fireEvent.change(input, { target: { value: 6 } })
         fireEvent.click(btn);
-        const output = screen.queryByText(/vi/i);
+        const output = screen.getByRole('heading', { name: /vi/i });
+
         //assert
         expect(input.value).toBe('6');
         expect(output).toBeInTheDocument();
     });
 
-    it('for the entry 25 it should renders a span XXV', () => {
-        //arrange
-        render(<Form />);
-
+    it('for the entry 25 it should renders a heading with the text XXV', () => {
         ////// compruebo que el mensaje no exista antes del evento ////
         expect(screen.queryByText(/xxv/i)).not.toBeInTheDocument();
         ////// compruebo que el mensaje no exista antes del evento ////
@@ -69,18 +63,16 @@ describe('given the form component, when the user submit the form', () => {
         //act
         const input = screen.getByPlaceholderText(/número/i);
         const btn = screen.getByRole('button', { name: /convertir/i });
-        fireEvent.change(input, { target: { value: 25 } })
+        fireEvent.change(input, { target: { value: 25 } });
         fireEvent.click(btn);
-        const output = screen.queryByText(/xxv/i);
+        const output = screen.getByRole('heading', { name: /xxv/i });
+
         //assert
         expect(input.value).toBe('25');
         expect(output).toBeInTheDocument();
     });
 
-    it('for the entry 4200 it should renders an error message', () => {
-        //arrange
-        render(<Form />);
-
+    it('for the entry 5200 it should renders an error message', () => {
         ////// compruebo que el mensaje no exista antes del evento ////
         expect(screen.queryByText(/el número ingresado es mayor que 3999, intenta con otro número/i)).not.toBeInTheDocument();
         ////// compruebo que el mensaje no exista antes del evento ////
@@ -88,18 +80,16 @@ describe('given the form component, when the user submit the form', () => {
         //act
         const input = screen.getByPlaceholderText(/número/i);
         const btn = screen.getByRole('button', { name: /convertir/i });
-        fireEvent.change(input, { target: { value: 4200 } })
+        fireEvent.change(input, { target: { value: 5200 } })
         fireEvent.click(btn);
         const errorMsg = screen.queryByText(/el número ingresado es mayor que 3999, intenta con otro número/i);
+
         //assert
-        expect(input.value).toBe('4200');
+        expect(input.value).toBe('5200');
         expect(errorMsg).toBeInTheDocument();
     });
 
-    it('for the entry e it should renders an error message', () => {
-        //arrange
-        render(<Form />);
-
+    it('for the entry e2 it should renders an error message', () => {
         ////// compruebo que el mensaje no exista antes del evento ////
         expect(screen.queryByText(/debes ingresar un número válido/i)).not.toBeInTheDocument();
         ////// compruebo que el mensaje no exista antes del evento ////
@@ -110,15 +100,13 @@ describe('given the form component, when the user submit the form', () => {
         fireEvent.change(input, { target: { value: 'e2' } })
         fireEvent.click(btn);
         const errorMsg = screen.queryByText(/debes ingresar un número válido/i);
+
         //assert
         expect(input.value).toBe('');
         expect(errorMsg).toBeInTheDocument();
     });
 
-    it('for the entry e it should renders an error message', () => {
-        //arrange
-        render(<Form />);
-
+    it('for the entry -55 it should renders an error message', () => {
         ////// compruebo que el mensaje no exista antes del evento ////
         expect(screen.queryByText(/debes ingresar un número válido/i)).not.toBeInTheDocument();
         ////// compruebo que el mensaje no exista antes del evento ////
@@ -129,6 +117,7 @@ describe('given the form component, when the user submit the form', () => {
         fireEvent.change(input, { target: { value: '-55' } })
         fireEvent.click(btn);
         const errorMsg = screen.queryByText(/debes ingresar un número válido/i);
+
         //assert
         expect(input.value).toBe('-55');
         expect(errorMsg).toBeInTheDocument();
